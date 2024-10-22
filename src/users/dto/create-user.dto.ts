f68a432b6daf055e5dfc873d5f0a698909682e75
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2024-10-21 21:08:56 -0300

    ajuste no DTO de user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,21 @@
-export class CreateUserDto {
+import { IsAlpha, IsEmail, IsNotEmpty, IsString, Length, MinLength } from "class-validator"
 
-    name:string
-    email:string  
-    password: string
+
+export class CreateUserDto {
+    @IsEmail({},{message:"Formato de email invalido"})
+    @IsNotEmpty({message:"Email não pode ser vazio"})
+    email: string;
+
+    @IsString({message:"Nome deve ser uma string"})
+    @IsNotEmpty({message: "Nome não pode ser vazio"})
+    @MinLength(3,{message:"Nome deve ter no mínimo 3 caracteres"})
+    name: string;
+
+    @IsAlpha()
+    @IsNotEmpty({message: "Senha não pode ser vazio"})
+    @MinLength(6,{message:"Senha deve ter no mínimo 6 caracteres"})
+    password: string;
+    
     isOnline: boolean
     profilePicture?:string
     
