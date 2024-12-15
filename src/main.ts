9c13b4c478ec0d87d7d38e555ebaf5492367759d
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2024-12-15 13:39:24 -0300

    fazendo deploy

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
-
-  await app.listen(3333);
+  const port = process.env.PORT
+  await app.listen(port);
 }
 bootstrap();
