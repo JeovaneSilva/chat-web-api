1756196896386025389ab6dec82d4c15a45735bc
Jeovane Silva <jeovane.silva.dev@gmail.com>
2025-01-09 13:03:16 -0300

    ajuste no main do projeto

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
     origin: ['https://chat-web-psi-indol.vercel.app'],
+    // origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
